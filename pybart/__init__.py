2a40924b4b1f92d8fde0f930fbcd6c8ff541009e
Anthony Ho <anwyho@gmail.com>
2018-08-23 11:02:35 -0700

    Housekeeping

--- pybart/__init__.py
+++ pybart/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+from . import updateStations
+
 import json
 import os
 import requests
---
+++ tests/test-pybart.py
@@ -0,0 +1,14 @@
+from .context import pybart
+
+import unittest
+
+class TestPybart(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_numbers_3_4(self):
+        self.assertEqual(3*4, 12)
+
+if __name__ == '__main__':
+    unittest.main()
